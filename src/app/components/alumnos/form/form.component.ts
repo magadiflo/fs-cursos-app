75076450af0fbeca89a8d0269e6a22afd5a91758
Martín <magadiflo@gmail.com>
2022-11-27 13:13:24 -0500

    126. Usando Formulario reactivo para crear alumno
    Esta nueva funcionalidad corresponde al capítulo 126. La directiva ngModel para mapear el formulario con el objeto model. En mi caso no usé NgModel (formularios por template), sino usé Formularios reactivos (Reactive Forms)

--- src/app/components/alumnos/form/form.component.ts
+++ src/app/components/alumnos/form/form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AlumnoService } from '../../../services/alumno.service';
 
 @Component({
   selector: 'app-form',
@@ -9,9 +13,28 @@ export class FormComponent implements OnInit {
 
   titulo: string = 'Crear Alumno';
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [null, [Validators.required]],
+    apellido: [null, [Validators.required]],
+    email: [null, [Validators.required, Validators.email]],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private alumnoService: AlumnoService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  crear(): void {
+    const data = { ...this.miFormulario.value }
+    this.alumnoService.crearAlumno(data)
+      .subscribe(alumno => {
+        console.log(alumno);
+        alert(`Alumno ${alumno.nombre} creado con éxito`);
+        this.router.navigate(['/alumnos']);
+      });
   }
 
 }
