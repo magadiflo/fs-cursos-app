53abdc9b3971c74133c089733d9cca216c67927a
Martín <magadiflo@gmail.com>
2022-12-04 13:18:39 -0500

    Escribiendo nuestro Componente Listar Common

--- src/app/components/alumnos/list/alumnos.component.ts
+++ src/app/components/alumnos/list/alumnos.component.ts
@@ -1,83 +1,25 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
-
-import Swal from 'sweetalert2';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 import { AlumnoService } from '../../../services/alumno.service';
 import { Alumno } from '../../../models/alumno';
+import { CommonListarComponent } from '../../common-listar.component';
+
+/**
+ * * El implements del OnInit y el AfterViewInit, lo podemos dejar aquí, no hay problema, ya
+ * * que finalmente se está implementando en el CommonListarComponent
+ */
 
 @Component({
   selector: 'app-alumnos',
   templateUrl: './alumnos.component.html',
   styleUrls: ['./alumnos.component.css']
 })
-export class AlumnosComponent implements OnInit, AfterViewInit {
+export class AlumnosComponent extends CommonListarComponent<Alumno, AlumnoService> implements OnInit, AfterViewInit {
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  titulo: string = 'Listado de Alumnos';
-  alumnos: Alumno[] = [];
-
-  totalRegistros: number = 0;
-  paginaActual: number = 0;
-  totalPorPagina: number = 4;
-  pageSizeOptions: number[] = [3, 5, 10, 25, 100];
-
-  constructor(private alumnoService: AlumnoService) { }
-
-  ngOnInit(): void {
-    this.listarPaginados();
-  }
-
-  ngAfterViewInit(): void {
-    this.intlLabels();
-  }
-
-  paginar(event: PageEvent): void {
-    this.paginaActual = event.pageIndex;
-    this.totalPorPagina = event.pageSize;
-    this.listarPaginados();
-  }
-
-  eliminar(alumno: Alumno): void {
-    Swal.fire({
-      title: `¿Seguro que desea eliminar al alumno ${alumno.nombre}?`,
-      text: "El alumno será eliminado de la BD",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Sí, eliminar'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.alumnoService.eliminar(alumno.id!)
-          .subscribe(() => {
-            //this.alumnos = this.alumnos.filter(a => a != alumno);
-            this.listarPaginados();
-            Swal.fire(
-              'Deleted!',
-              `Alumno ${alumno.nombre} eliminado con éxito`,
-              'success'
-            );
-          });
-      }
-    });
-  }
-
-  private listarPaginados(): void {
-    this.alumnoService.listarPaginas(this.paginaActual, this.totalPorPagina)
-      .subscribe(pagination => {
-        this.alumnos = pagination.content;
-        this.totalRegistros = pagination.totalElements;
-      });
-  }
-
-  private intlLabels() {
-    this.paginator._intl.itemsPerPageLabel = 'Registros por página'; //* _intl: internacionalización
-    this.paginator._intl.firstPageLabel = 'Primera página';
-    this.paginator._intl.lastPageLabel = 'Última página';
-    this.paginator._intl.nextPageLabel = 'Página siguiente';
-    this.paginator._intl.previousPageLabel = 'Página anterior';
+  constructor(service: AlumnoService) {
+    super(service);
+    this.titulo = 'Listar alumnos';
+    this.nombreModel = Alumno.name; //* Se obtiene el nombre de la Clase
   }
 
 }
---
+++ src/app/components/common-listar.component.ts
@@ -0,0 +1,79 @@
+import { OnInit, ViewChild, AfterViewInit, Directive } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+
+import Swal from 'sweetalert2';
+
+import { Generic } from '../models/generic';
+import { CommonService } from '../services/common.service';
+
+@Directive() //* Con esto se soluciona el problema de: "Class is using Angular features but is not decorated. Please add an explicit Angular decorator"
+export abstract class CommonListarComponent<E extends Generic, S extends CommonService<E>> implements OnInit, AfterViewInit {
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  protected nombreModel: string = '';
+  protected titulo: string = '';
+  protected lista: E[] = [];
+
+  totalRegistros: number = 0;
+  paginaActual: number = 0;
+  totalPorPagina: number = 4;
+  pageSizeOptions: number[] = [3, 5, 10, 25, 100];
+
+  constructor(protected service: S) { }
+
+  ngOnInit(): void {
+    this.listarPaginados();
+  }
+
+  ngAfterViewInit(): void {
+    this.intlLabels();
+  }
+
+  paginar(event: PageEvent): void {
+    this.paginaActual = event.pageIndex;
+    this.totalPorPagina = event.pageSize;
+    this.listarPaginados();
+  }
+
+  eliminar(e: E): void {
+    Swal.fire({
+      title: `¿Seguro que desea eliminar al ${this.nombreModel} ${e.nombre}?`,
+      text: `El ${this.nombreModel} será eliminado de la BD`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.service.eliminar(e.id!)
+          .subscribe(() => {
+            this.listarPaginados();
+            Swal.fire(
+              'Deleted!',
+              `${this.nombreModel} ${e.nombre} eliminado con éxito`,
+              'success'
+            );
+          });
+      }
+    });
+  }
+
+  private listarPaginados(): void {
+    this.service.listarPaginas(this.paginaActual, this.totalPorPagina)
+      .subscribe(pagination => {
+        this.lista = pagination.content;
+        this.totalRegistros = pagination.totalElements;
+      });
+  }
+
+  private intlLabels() {
+    this.paginator._intl.itemsPerPageLabel = 'Registros por página'; //* _intl: internacionalización
+    this.paginator._intl.firstPageLabel = 'Primera página';
+    this.paginator._intl.lastPageLabel = 'Última página';
+    this.paginator._intl.nextPageLabel = 'Página siguiente';
+    this.paginator._intl.previousPageLabel = 'Página anterior';
+  }
+
+}
