e4c7006fe1deca14f8d56e4881bfd12d6e81ffea
Martín <magadiflo@gmail.com>
2022-12-04 12:31:43 -0500

    Extendiendo de nuestro Service Common en CursoService y ExamenService

--- src/app/services/curso.service.ts
+++ src/app/services/curso.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { CommonService } from './common.service';
+import { Curso } from '../models/curso';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CursoService {
+export class CursoService extends CommonService<Curso> {
 
-  constructor() { }
+  protected override baseEndPoint: string = 'http://localhost:8090/api/cursos';
+
+  constructor(protected override http: HttpClient) {
+    super(http);
+  }
+
 }
--- src/app/services/examen.service.ts
+++ src/app/services/examen.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Examen } from '../models/examen';
+import { CommonService } from './common.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ExamenService {
+export class ExamenService extends CommonService<Examen> {
 
-  constructor() { }
+  protected override baseEndPoint: string = 'http://localhost:8090/api/examenes';
+
+  constructor(protected override http: HttpClient) {
+    super(http);
+  }
+
 }
