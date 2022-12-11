c4dee4e0693ac508ed02743339fad359790c863a
Martín <magadiflo@gmail.com>
2022-12-11 18:14:09 -0500

    Añadiendo evento del autocomplete para filtrar en el ngOnInit

--- src/app/components/cursos/asignar-examenes/asignar-examenes.component.ts
+++ src/app/components/cursos/asignar-examenes/asignar-examenes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { switchMap } from 'rxjs/operators';
+import { map } from 'rxjs';
 
 import { Curso } from '../../../models/curso';
 import { Examen } from '../../../models/examen';
@@ -32,6 +33,17 @@ export class AsignarExamenesComponent implements OnInit {
         switchMap(({ id }) => this.cursoService.ver(id))
       )
       .subscribe(curso => console.log(curso));
+
+    this.autocompleteControl.valueChanges
+      .pipe(
+        map(valor => typeof valor === 'string' ? valor : valor.nombre),
+        switchMap(valor => valor ? this.examenService.filtrarPorNombre(valor) : [])
+      )
+      .subscribe(examenes => this.examenesFiltrados = examenes);
+  }
+
+  mostrarNombre(examen?: Examen): string {
+    return examen ? examen.nombre : '';
   }
 
 }
